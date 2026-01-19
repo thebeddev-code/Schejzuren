9d843a79d7f11c996737944b15ad1fe97fa4174c
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-01-19 13:46:40 +0500

    feat: add sqlite database support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use tauri::App;
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,7 +9,37 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![
+    Migration {
+        version: 1,
+        description: "create todos table",
+        sql: "CREATE TABLE IF NOT EXISTS todos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT,
+            tags TEXT,
+            color TEXT,
+            status TEXT NOT NULL CHECK(status IN ('pending', 'in-progress', 'completed', 'overdue')),
+            priority TEXT NOT NULL CHECK(priority IN ('low', 'medium', 'high')),
+            startsAt TEXT,
+            due TEXT,
+            updatedAt TEXT NOT NULL,
+            completedAt TEXT,
+            isRecurring INTEGER NOT NULL,
+            recurrenceRule TEXT
+        )",
+        kind: MigrationKind::Up,
+    }
+]
+    ;
+
     tauri::Builder::default()
+        .manage({ AppData { db_pool } })
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:todos.db", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
