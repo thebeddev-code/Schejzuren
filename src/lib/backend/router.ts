f8fc82ee90a286b9d4d2ac2c82bdb0bc59e5eb56
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-01-26 17:38:42 +0500

    feat(backend): implement type to extract route handler args and return types

--- src/lib/backend/router.ts
+++ src/lib/backend/router.ts
@@ -1,22 +1,48 @@
 import createRouteMatcher from '@tscircuit/routematch'
 import { handleGetTodos } from './controllers/todo.controller';
 
-export type Options = Record<string, unknown>
-const routerConfig = {
+export const routerConfig = {
   "/todos": {
     GET: handleGetTodos
   }
 }
-type Routes = keyof typeof routerConfig;
+
+export type Endpoints = keyof typeof routerConfig;
+
+// Infering route options based off route handler
+// Essentially what this type does, it extracts route handler arguments
+export type EndpointOptions<
+  E extends Endpoints,
+  M extends keyof typeof routerConfig[E]
+> = Parameters<(typeof routerConfig)[E][M] extends (...args: any[]) => any
+  ? (typeof routerConfig)[E][M]
+  : never>[0];
+
+export type EndpointResponse<
+  E extends Endpoints,
+  M extends keyof typeof routerConfig[E]
+> = ReturnType<(typeof routerConfig)[E][M] extends (...args: any[]) => any
+  ? (typeof routerConfig)[E][M]
+  : never>[0];
+
+export type EndpointDescription<
+  E extends Endpoints,
+  M extends keyof typeof routerConfig[E]
+> = {
+  options: EndpointOptions<E, M>;
+  response: EndpointResponse<E, M>;
+};
+
 
 const routeMatcher = createRouteMatcher(Object.keys(routerConfig))
 
-export function handleRequest(method: string, route: string, options?: Options) {
+// Simple routing
+export function handleRequest<Routes>(method: string, route: string, options: unknown) {
   const result = routeMatcher(route);
   if (!result) {
     throw new Error(`Route [${route}] is not implemented`)
   }
-  const handler = routerConfig[result.matchedRoute as Routes][method];
+  const handler = routerConfig[result.matchedRoute][method];
   if (!handler) {
     throw new Error(`Method [${method}] is not implemented for [${route}] Route`)
   }
