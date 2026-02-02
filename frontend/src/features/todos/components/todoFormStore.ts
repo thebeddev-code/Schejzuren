04fe02c69199b3217a04a32aef27d794fb424597
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-02 17:43:30 +0500

    feat(todos): abstract todo form store setter
    This helps to centralize todo form store state management
    Which helps in reducing redundancy and potential bugs

--- frontend/src/features/todos/components/todoFormStore.ts
+++ frontend/src/features/todos/components/todoFormStore.ts
@@ -1,12 +1,46 @@
+import { parseColor } from "@kobalte/core/colors";
 import { createStore } from "solid-js/store";
 import { Todo } from "~/lib/types";
 
+const CREATE_TODO_DEFAULT_DATA = {
+  title: "",
+  isRecurring: false,
+  tags: [],
+  createdAt: new Date().toISOString(),
+  description: "",
+  priority: "low",
+  status: "pending",
+  updatedAt: new Date().toISOString(),
+  due: new Date().toISOString(),
+  startsAt: new Date().toISOString(),
+  recurrenceRule: "",
+  monthlyDate: new Date().toString(),
+  color: "rgba(255,255,255,0)"
+}
+
+export type FormTypes = "create" | "update" | null
 
 export type TodoFormStore = {
-  formType: "create" | "update" | "readonly" | null
+  formType: FormTypes
   todoData?: Partial<Todo> | null
 }
-// Initialize store
+
 export const [todoFormStore, setTodoFormStore] = createStore<TodoFormStore>({
   formType: null,
 })
+
+export function openTodoForm(formType: FormTypes, todo?: Todo) {
+  if (formType === "create") {
+    return setTodoFormStore({
+      formType,
+      todoData: todo ? { ...CREATE_TODO_DEFAULT_DATA, ...todo } : CREATE_TODO_DEFAULT_DATA
+    })
+  }
+  if (formType === "update") {
+    return setTodoFormStore({
+      formType,
+      todoData: CREATE_TODO_DEFAULT_DATA as unknown as Todo
+    })
+  }
+}
+
