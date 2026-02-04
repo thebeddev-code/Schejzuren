c3f4ea0ff0c59b049641e7e16ae0035c15da3ee1
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-04 18:38:22 +0500

    chore: regen go-js-bindings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { createSignal } from "solid-js";
 import logo from "./assets/logo.svg";
 
-function App() {return (
+function App() {
+	return (
 		<main class="container">
 			<div class="w-10 bg-black" />
 		</main>
--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,40 +1,43 @@
 export namespace services {
+	
 	export class Todo {
-		id: number;
-		title: string;
-		description?: string;
-		tags: string[];
-		color?: string;
-		status: string;
-		priority: string;
-		startsAt?: string;
-		due?: string;
-		updatedAt?: string;
-		createdAt?: string;
-		completedAt?: string;
-		isRecurring: boolean;
-		recurrenceRule?: string;
-
-		static createFrom(source: any = {}) {
-			return new Todo(source);
-		}
-
-		constructor(source: any = {}) {
-			if ("string" === typeof source) source = JSON.parse(source);
-			this.id = source["id"];
-			this.title = source["title"];
-			this.description = source["description"];
-			this.tags = source["tags"];
-			this.color = source["color"];
-			this.status = source["status"];
-			this.priority = source["priority"];
-			this.startsAt = source["startsAt"];
-			this.due = source["due"];
-			this.updatedAt = source["updatedAt"];
-			this.createdAt = source["createdAt"];
-			this.completedAt = source["completedAt"];
-			this.isRecurring = source["isRecurring"];
-			this.recurrenceRule = source["recurrenceRule"];
-		}
+	    id: number;
+	    title: string;
+	    description?: string;
+	    tags: string[];
+	    color?: string;
+	    status: string;
+	    priority: string;
+	    startsAt?: string;
+	    due?: string;
+	    updatedAt?: string;
+	    createdAt?: string;
+	    completedAt?: string;
+	    isRecurring: boolean;
+	    recurrenceRule?: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Todo(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.title = source["title"];
+	        this.description = source["description"];
+	        this.tags = source["tags"];
+	        this.color = source["color"];
+	        this.status = source["status"];
+	        this.priority = source["priority"];
+	        this.startsAt = source["startsAt"];
+	        this.due = source["due"];
+	        this.updatedAt = source["updatedAt"];
+	        this.createdAt = source["createdAt"];
+	        this.completedAt = source["completedAt"];
+	        this.isRecurring = source["isRecurring"];
+	        this.recurrenceRule = source["recurrenceRule"];
+	    }
 	}
+
 }
+
