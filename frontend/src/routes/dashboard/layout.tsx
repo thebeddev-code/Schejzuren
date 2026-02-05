c09ef6789c331a5cdfbf42c23d63728b8484acd2
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-05 15:16:47 +0500

    feat(main): add toast region

--- frontend/src/routes/dashboard/layout.tsx
+++ frontend/src/routes/dashboard/layout.tsx
@@ -1,5 +1,13 @@
+import { Toast } from "@kobalte/core/toast";
 import type { ParentProps } from "solid-js";
 
 export default function Layout(props: ParentProps) {
-	return <>{props.children}</>;
+	return (
+		<div class="relative">
+			<Toast.Region duration={4000} class="w-full flex justify-center">
+				<Toast.List class="fixed top-0 flex flex-col gap-2 p-4 w-96 max-w-dvw m-0 list-none z-50 outline-none" />
+			</Toast.Region>
+			{props.children}
+		</div>
+	);
 }
