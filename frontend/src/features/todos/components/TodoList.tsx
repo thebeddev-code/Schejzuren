7f8550fbf795515fbcea1ae5c60949a8577d2fbb
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-05 11:41:53 +0500

    refactor(todos): improve TodoList and lift the edit todo click
    - Improved semantics
    - Removed dead code: ExpandedTodoView, unnecessary state

--- frontend/src/features/todos/components/TodoList.tsx
+++ frontend/src/features/todos/components/TodoList.tsx
@@ -1,41 +1,33 @@
 import { Plus } from "lucide-solid";
+import { type Accessor, For } from "solid-js";
 import { Button } from "~/lib/components/ui/button";
 import type { Todo } from "~/lib/types";
-import { TodoExpandedView } from "./TodoExpandedView";
 import { TodoItem } from "./TodoItem";
-import { type Accessor, createMemo, createSignal, For, Show } from "solid-js";
-import { openTodoForm, setTodoFormStore } from "./todoFormStore";
+import { openTodoForm } from "./todoFormStore";
 
 export function TodoList({ todos }: { todos: Accessor<Todo[]> }) {
-	const [expandedTodoId, setExpandedTodoId] = createSignal<null | number>(null);
-
-	const expandedTodo = createMemo(() =>
-		todos().find((t) => t.id == expandedTodoId()),
-	);
-	function handleShowTodoExpandedView(todoId: number) {
-		setExpandedTodoId(todoId);
-	}
 	return (
-		<div class="flex h-dvh w-full flex-col gap-2 overflow-y-auto rounded-lg bg-gray-50 p-4 border border-gray-200">
-			<Show when={expandedTodo()}>
-				<div
-					onClick={() => setExpandedTodoId(null)}
-					class="fixed inset-0 z-20 flex items-center justify-center bg-black/40"
-				>
-					<TodoExpandedView
-						todo={expandedTodo() as Todo}
-						onExpandedViewClose={() => setExpandedTodoId(null)}
-					/>
-				</div>
-			</Show>
-			<For each={todos()}>
-				{(item) => (
-					<TodoItem
-						todo={item}
-						onShowExpandedView={handleShowTodoExpandedView}
-					/>
-				)}
-			</For>
+		<section class="flex h-dvh w-full flex-col gap-2 overflow-y-auto rounded-lg bg-gray-50 p-4 border border-gray-200">
+			<ul>
+				<For each={todos()}>
+					{(item) => (
+						<li
+							onClick={(e) => {
+								e.stopPropagation();
+								openTodoForm("update", item);
+							}}
+							onKeyDown={(e) => {
+								if (e.key === "Enter" || e.key === " ") {
+									e.preventDefault();
+									openTodoForm("update", item);
+								}
+							}}
+						>
+							<TodoItem todo={item} />
+						</li>
+					)}
+				</For>
+			</ul>
 
 			<div class="flex justify-center">
 				<Button
@@ -46,6 +38,6 @@ export function TodoList({ todos }: { todos: Accessor<Todo[]> }) {
 					<Plus class="text-slate-800" />
 				</Button>
 			</div>
-		</div>
+		</section>
 	);
 }
