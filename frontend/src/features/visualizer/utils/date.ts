ad145525f41c87456ed714562bd962b0675a9521
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-11 16:38:37 +0500

    feat(utils): implement util that formats hours into AM/PM

--- frontend/src/features/visualizer/utils/date.ts
+++ frontend/src/features/visualizer/utils/date.ts
@@ -1,4 +1,4 @@
-import { addHours, set } from "date-fns";
+import { addHours, format, set } from "date-fns";
 import { DEGREES_PER_HOUR } from "./constants";
 
 export function degreesToDate(degrees: number, startDate = new Date()) {
@@ -6,4 +6,14 @@ export function degreesToDate(degrees: number, startDate = new Date()) {
 		set(startDate, { hours: 0, minutes: 0, seconds: 0 }),
 		degrees / DEGREES_PER_HOUR,
 	);
+}
+
+export function formatTimeToAmPm(
+	hours: number,
+	minutes: number = 0,
+	seconds: number = 0,
+): string {
+	const date = new Date();
+	date.setHours(hours, minutes, seconds, 0);
+	return format(date, "h:mm a");
 }
