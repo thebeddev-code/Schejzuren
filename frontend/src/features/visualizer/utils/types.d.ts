777fe6c283f6bff3884d7f85a61f4194f1ea5b94
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-11 17:05:56 +0500

    fix(visualizer): make Activity item fields optional for Drawable & VisualizableItem

--- frontend/src/features/visualizer/utils/types.d.ts
+++ frontend/src/features/visualizer/utils/types.d.ts
@@ -1,12 +1,12 @@
 import { Activity } from "~/lib/types";
 
-export type VisualizableItem = Activity;
+export type VisualizableItem = Partial<Activity>;
 
 export type Drawable = {
 	startTimeHours: number;
 	endTimeHours: number;
 	color: string;
-} & Omit<Activity, "startsAt" | "due" | "color">;
+} & Partial<Omit<Activity, "startsAt" | "due" | "color">>;
 
 export type Vector2 = {
 	x: number;
