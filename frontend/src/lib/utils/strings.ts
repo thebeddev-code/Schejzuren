01f3336b3608bc1b65ef152bae99273b1cb68820
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-11 16:37:30 +0500

    feat(utils): implement util that shortens strings

--- frontend/src/lib/utils/strings.ts
+++ frontend/src/lib/utils/strings.ts
@@ -1,1 +1,6 @@
 export const trimAndLowercase = (s: string) => s.trim().toLowerCase();
+
+export const shortenString = (s: string, maxLen: number) => {
+	if (s.length > maxLen) return `${s.slice(0, maxLen)}...`;
+	return s;
+};
