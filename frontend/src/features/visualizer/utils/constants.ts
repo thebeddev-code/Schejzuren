a2f3dd7b8780f1166fb3c027cadeb62df346ff65
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-14 18:51:45 +0500

    refactor(visualizer): add constants for hours per day/day part

--- frontend/src/features/visualizer/utils/constants.ts
+++ frontend/src/features/visualizer/utils/constants.ts
@@ -1,3 +1,9 @@
 export const DEGREES_PER_HOUR = 30;
+
+export const HOURS_PER_DAY = 24;
+
 export const COLOR_WHEEL_WIDTH = 100;
+
 export const VISUALIZABLE_ARC_WIDTH = 40;
+
+export const DAY_PART_HOURS = 6;
