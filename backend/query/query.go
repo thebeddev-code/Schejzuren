e6277b092579b1bcc0fcd4ccd5ac7ebde4c3d8ab
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-19 17:48:11 +0500

    feat(backend): add support for complex queries
    Now logical operators: AND, OR, NOT are supported

--- backend/query/query.go
+++ backend/query/query.go
@@ -4,7 +4,8 @@ package query
 import (
 	"fmt"
 	"reflect"
-	"strings"
+
+	"schejzuren/backend/utils"
 
 	"gorm.io/gorm"
 )
@@ -35,12 +36,13 @@ type BaseQuery struct {
 }
 
 type ItemFilter struct {
-	ID    *FilterCondition[uint]   `json:"id,omitempty"`
-	Title *FilterCondition[string] `json:"title,omitempty"`
+	ID             *FilterCondition[uint]   `json:"id,omitempty"`
+	Title          *FilterCondition[string] `json:"title,omitempty"`
+	RecurrenceRule *FilterCondition[string] `json:"recurrenceRule,omitempty"`
 
-	// AND []*ItemFilter `json:"AND,omitempty"`
-	// OR  []*ItemFilter `json:"OR,omitempty"`
-	// NOT *ItemFilter   `json:"NOT,omitempty"`
+	AND []*ItemFilter `json:"AND,omitempty"`
+	OR  []*ItemFilter `json:"OR,omitempty"`
+	NOT *ItemFilter   `json:"NOT,omitempty"`
 }
 
 type ItemQuery struct {
@@ -48,14 +50,24 @@ type ItemQuery struct {
 	Where *ItemFilter `json:"where,omitempty"`
 }
 
+type QueryType int
+
+const (
+	queryAND QueryType = iota
+	queryOR
+	queryNOT
+)
+
 func ApplyItemQuery(q *ItemQuery) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		if q == nil {
 			return db
 		}
 
-		// Pagination
+		// Apply pagination
 		if q.Paginate != nil {
+
+			// Use default page of 100 if not size is provided
 			size := q.Paginate.Size
 			if size <= 0 {
 				size = 100
@@ -63,15 +75,16 @@ func ApplyItemQuery(q *ItemQuery) func(db *gorm.DB) *gorm.DB {
 
 			db = db.Limit(size)
 
-			if q.Paginate.Page > 0 {
-				offset := (q.Paginate.Page - 1) * size
-				db = db.Offset(offset)
-			} else if q.Paginate.Offset > 0 {
-				db = db.Offset(q.Paginate.Offset)
+			// Calculate items offset
+			offset := q.Paginate.Offset
+			if q.Paginate.Page > 1 {
+				offset += (q.Paginate.Page - 1) * size
 			}
+
+			db = db.Offset(offset)
 		}
 
-		// Sorting
+		// Apply sorting
 		for _, field := range q.Asc {
 			db = db.Order(fmt.Sprintf("%s ASC", field))
 		}
@@ -79,9 +92,9 @@ func ApplyItemQuery(q *ItemQuery) func(db *gorm.DB) *gorm.DB {
 			db = db.Order(fmt.Sprintf("%s DESC", field))
 		}
 
-		// Filtering
+		// Apply filtering
 		if q.Where != nil {
-			db = buildWhere(db, q.Where)
+			db = buildWhere(db, q.Where, queryAND)
 		}
 
 		return db
@@ -89,62 +102,51 @@ func ApplyItemQuery(q *ItemQuery) func(db *gorm.DB) *gorm.DB {
 }
 
 // buildWhere recursively generates the GORM clauses
-func buildWhere(db *gorm.DB, f *ItemFilter) *gorm.DB {
+func buildWhere(db *gorm.DB, f *ItemFilter, queryType QueryType) *gorm.DB {
 	if f == nil {
 		return db
 	}
 
-	db = applyFilter(db, f, nil)
-	// FIXME: broken filtering
+	// Apply filters for current fields and query type
+	db = applyFilter(db, f, queryType)
 
-	// for _, sub := range f.AND {
-	// 	db = db.Where(func(tx *gorm.DB) *gorm.DB {
-	// 		return buildWhere(tx, sub)
-	// 	})
-	// }
-	//
-	// if len(f.OR) > 0 {
-	// 	db = db.Where(func(tx *gorm.DB) *gorm.DB {
-	// 		for i, sub := range f.OR {
-	// 			if i == 0 {
-	// 				tx = buildWhere(tx, sub)
-	// 			} else {
-	// 				tx = tx.Or(func(tx2 *gorm.DB) *gorm.DB {
-	// 					return buildWhere(tx2, sub)
-	// 				})
-	// 			}
-	// 		}
-	// 		return tx
-	// 	})
-	// }
-	//
-	// if f.NOT != nil {
-	// 	db = db.Not(func(tx *gorm.DB) *gorm.DB {
-	// 		return buildWhere(tx, f.NOT)
-	// 	})
-	// }
+	// Recursively apply logical operators
+	for _, sub := range f.AND {
+		db = buildWhere(db, sub, queryAND)
+	}
+
+	for _, sub := range f.OR {
+		db = buildWhere(db, sub, queryOR)
+	}
+
+	if f.NOT != nil {
+		db = buildWhere(db, f.NOT, queryNOT)
+	}
 
 	return db
 }
 
-func applyFilter[T any](db *gorm.DB, f *T, fieldMap map[string]string) *gorm.DB {
-	if f == nil {
+func applyFilter[T any](db *gorm.DB, filters *T, queryType QueryType) *gorm.DB {
+	if filters == nil {
 		return db
 	}
 
-	rv := reflect.ValueOf(f)
-	if rv.Kind() != reflect.Pointer || rv.IsNil() {
+	// Check if [filters] a valid pointer
+	ptr := reflect.ValueOf(filters)
+	if ptr.Kind() != reflect.Pointer || ptr.IsNil() {
 		return db
 	}
 
-	v := rv.Elem()
-	if v.Kind() != reflect.Struct {
+	// Deref and check if struct
+	filtersValue := ptr.Elem()
+	if filtersValue.Kind() != reflect.Struct {
 		return db
 	}
 
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Field(i)
-		fieldName := v.Type().Field(i).Name
+	// Go over filters
+	for i := 0; i < filtersValue.NumField(); i++ {
+		field := filtersValue.Field(i)
+		fieldName := filtersValue.Type().Field(i).Name
 
 		if fieldName == "AND" || fieldName == "OR" || fieldName == "NOT" {
 			continue
@@ -154,65 +156,73 @@ func applyFilter[T any](db *gorm.DB, f *T, fieldMap map[string]string) *gorm.DB 
 			continue
 		}
 
-		// Map Go field name to DB column
-		dbField := fieldMap[fieldName]
-		if dbField == "" {
-			dbField = strings.ToLower(fieldName)
-		}
-
-		db = applyCondition(db, dbField, field.Interface())
+		db = applyCondition(db, utils.ToSnakeCase(fieldName), field.Interface(), queryType)
 	}
 
 	return db
 }
 
-func applyCondition(db *gorm.DB, column string, cond any) *gorm.DB {
+func applyCondition(db *gorm.DB, column string, cond any, queryType QueryType) *gorm.DB {
 	if cond == nil {
 		return db
 	}
 
+	// Match filter condition type
 	switch c := cond.(type) {
 	case *FilterCondition[uint]:
-		return applyConditionGeneric(db, column, c)
+		return applyConditionGeneric(db, column, c, queryType)
 	case *FilterCondition[string]:
-		return applyConditionGeneric(db, column, c)
+		return applyConditionGeneric(db, column, c, queryType)
 	case *FilterCondition[int]:
-		return applyConditionGeneric(db, column, c)
+		return applyConditionGeneric(db, column, c, queryType)
 	default:
 		return db
 	}
 }
 
-func applyConditionGeneric[T any](db *gorm.DB, column string, c *FilterCondition[T]) *gorm.DB {
+func applyConditionGeneric[T any](db *gorm.DB, column string, c *FilterCondition[T], queryType QueryType) *gorm.DB {
+	cond := func(format string, args ...interface{}) *gorm.DB {
+		switch queryType {
+		case queryOR:
+			return db.Or(format, args...)
+		case queryNOT:
+			return db.Not(fmt.Sprintf(format, column), args...)
+		case queryAND:
+			fallthrough
+		default:
+			return db.Where(format, args...)
+		}
+	}
+
 	if c.Eq != nil {
-		return db.Where(fmt.Sprintf("%s = ?", column), *c.Eq)
+		return cond(fmt.Sprintf("%s = ?", column), *c.Eq)
 	}
 	if c.Ne != nil {
-		return db.Where(fmt.Sprintf("%s <> ?", column), *c.Ne)
+		return cond(fmt.Sprintf("%s <> ?", column), *c.Ne)
 	}
 	if c.Gt != nil {
-		return db.Where(fmt.Sprintf("%s > ?", column), *c.Gt)
+		return cond(fmt.Sprintf("%s > ?", column), *c.Gt)
 	}
 	if c.Gte != nil {
-		return db.Where(fmt.Sprintf("%s >= ?", column), *c.Gte)
+		return cond(fmt.Sprintf("%s >= ?", column), *c.Gte)
 	}
 	if c.Lt != nil {
-		return db.Where(fmt.Sprintf("%s < ?", column), *c.Lt)
+		return cond(fmt.Sprintf("%s < ?", column), *c.Lt)
 	}
 	if c.Lte != nil {
-		return db.Where(fmt.Sprintf("%s <= ?", column), *c.Lte)
+		return cond(fmt.Sprintf("%s <= ?", column), *c.Lte)
 	}
 	if len(c.In) > 0 {
-		return db.Where(fmt.Sprintf("%s IN ?", column), c.In)
+		return cond(fmt.Sprintf("%s IN ?", column), c.In)
 	}
 	if len(c.NotIn) > 0 {
-		return db.Where(fmt.Sprintf("%s NOT IN ?", column), c.NotIn)
+		return cond(fmt.Sprintf("%s NOT IN ?", column), c.NotIn)
 	}
 	if c.Like != nil {
-		return db.Where(fmt.Sprintf("%s LIKE ?", column), *c.Like)
+		return cond(fmt.Sprintf("%s LIKE ?", column), *c.Like)
 	}
 	if c.Contains != nil {
-		return db.Where(fmt.Sprintf("%s LIKE ?", column), "%"+*c.Contains+"%")
+		return cond(fmt.Sprintf("%s LIKE ?", column), "%"+*c.Contains+"%")
 	}
 	return db
 }
