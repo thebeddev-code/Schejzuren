7d4d806f8433b426367cd369065ee2c8f5d53187
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-20 14:01:54 +0500

    refactor: update routes and add new routes
    - Renamed activities route file to today
    - Added new activity routes: all, calendar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,9 @@ import { Route, Router } from "@solidjs/router";
 import { render } from "solid-js/web";
 import "./index.css";
 import { applyTheme } from "./lib/utils/theme.ts";
-import Activities from "./routes/dashboard/activities/activities.tsx";
+import ActivitiesAll from "./routes/dashboard/activities/all.tsx";
+import ActivitiesCalendar from "./routes/dashboard/activities/calendar.tsx";
+import ActivitiesToday from "./routes/dashboard/activities/today.tsx";
 import DashboardLayout from "./routes/layout.tsx";
 import Settings from "./routes/settings.tsx";
 
@@ -12,9 +14,13 @@ applyTheme();
 render(
 	() => (
 		<Router root={DashboardLayout}>
-			<Route path="/" component={Activities} />
-			<Route path="/calendar" />
-			<Route path="/settings" component={Settings} />
+			<Route path="/dashboard/activities" component={ActivitiesAll} />
+			<Route path="/dashboard/activities/today" component={ActivitiesToday} />
+			<Route
+				path="/dashboard/activities/calendar"
+				component={ActivitiesCalendar}
+			/>
+			<Route path="/dashboard/settings" component={Settings} />
 		</Router>
 	),
 	document.getElementById("root") as HTMLElement,
