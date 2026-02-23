2202b2ea0ca5c205bf955d74787b2de38d92cd7a
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-23 20:50:59 +0500

    feat: render a list of activities in the all activities route

--- frontend/src/routes/dashboard/activities/all.tsx
+++ frontend/src/routes/dashboard/activities/all.tsx
@@ -1,3 +1,16 @@
+import { createAsync } from "@solidjs/router";
+import { createMemo } from "solid-js";
+import { ActivitiesList } from "~/features/activities/components/ActivitiesList";
+import { getActivities } from "~/lib/controllers/activity.controller";
+
 export default function ActivitiesAll() {
-	return <></>;
+	const activitiesQueryResult = createAsync(() => getActivities({}));
+	const activities = createMemo(() => {
+		return activitiesQueryResult() ?? [];
+	});
+	return (
+		<main class="flex justify-center w-full">
+			<ActivitiesList activities={activities} />
+		</main>
+	);
 }
